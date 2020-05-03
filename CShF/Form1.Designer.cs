1333bdaa8ddf8c623194426379762463152b267c
Denis Korkishko <Game.den20@yandex.ru>
2020-05-04 00:56:53 +0300

    Добавьте файлы проекта.

---
+++ CShF/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace CShF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.x3 = new System.Windows.Forms.TextBox();
+            this.y3 = new System.Windows.Forms.TextBox();
+            this.y2 = new System.Windows.Forms.TextBox();
+            this.y1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.rez = new System.Windows.Forms.TextBox();
+            this.rez_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(50, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Периметр треугольника";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "x1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(155, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "x2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(299, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "x3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "y1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(155, 115);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 18);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "y2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(299, 115);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 18);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "y3";
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(41, 71);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(100, 20);
+            this.x1.TabIndex = 7;
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(184, 72);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(100, 20);
+            this.x2.TabIndex = 8;
+            // 
+            // x3
+            // 
+            this.x3.Location = new System.Drawing.Point(328, 72);
+            this.x3.Name = "x3";
+            this.x3.Size = new System.Drawing.Size(100, 20);
+            this.x3.TabIndex = 9;
+            // 
+            // y3
+            // 
+            this.y3.Location = new System.Drawing.Point(328, 115);
+            this.y3.Name = "y3";
+            this.y3.Size = new System.Drawing.Size(100, 20);
+            this.y3.TabIndex = 10;
+            // 
+            // y2
+            // 
+            this.y2.Location = new System.Drawing.Point(184, 116);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(100, 20);
+            this.y2.TabIndex = 11;
+            // 
+            // y1
+            // 
+            this.y1.Location = new System.Drawing.Point(41, 116);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(100, 20);
+            this.y1.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(136, 169);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 18);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Результат -";
+            // 
+            // rez
+            // 
+            this.rez.Location = new System.Drawing.Point(222, 167);
+            this.rez.Name = "rez";
+            this.rez.ReadOnly = true;
+            this.rez.Size = new System.Drawing.Size(206, 20);
+            this.rez.TabIndex = 14;
+            // 
+            // rez_button
+            // 
+            this.rez_button.Location = new System.Drawing.Point(172, 217);
+            this.rez_button.Name = "rez_button";
+            this.rez_button.Size = new System.Drawing.Size(85, 29);
+            this.rez_button.TabIndex = 15;
+            this.rez_button.Text = "Вычислить";
+            this.rez_button.UseVisualStyleBackColor = true;
+            this.rez_button.Click += new System.EventHandler(this.rez_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 258);
+            this.Controls.Add(this.rez_button);
+            this.Controls.Add(this.rez);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.y3);
+            this.Controls.Add(this.x3);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.x1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox x1;
+        private System.Windows.Forms.TextBox x2;
+        private System.Windows.Forms.TextBox x3;
+        private System.Windows.Forms.TextBox y3;
+        private System.Windows.Forms.TextBox y2;
+        private System.Windows.Forms.TextBox y1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox rez;
+        private System.Windows.Forms.Button rez_button;
+    }
+}
+
---
+++ CShF/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CShF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void rez_button_Click(object sender, EventArgs e)
+        {
+            double X1, Y1, X2, Y2, X3, Y3, re, AB, AC, BC;
+
+            X1 = Convert.ToDouble(x1.Text);
+            Y1 = Convert.ToDouble(y1.Text);
+
+            X2 = Convert.ToDouble(x2.Text);
+            Y2 = Convert.ToDouble(y2.Text);
+
+            X3 = Convert.ToDouble(x3.Text);
+            Y3 = Convert.ToDouble(y3.Text);
+
+            AB = Math.Sqrt(((X2 - X1) * (X2 - X1)) + ((Y2 - Y1) * (Y2 - Y1)));
+            AC = Math.Sqrt(((X3 - X1) * (X3 - X1)) + ((Y3 - Y1) * (Y3 - Y1)));
+            BC = Math.Sqrt(((X3 - X2) * (X3 - X2)) + ((Y3 - Y2) * (Y3 - Y2)));
+
+            re = AB + BC + AC;
+
+            rez.Text = Convert.ToString(re);
+        }
+    }
+}
